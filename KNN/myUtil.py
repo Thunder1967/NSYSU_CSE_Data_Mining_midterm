6dc11de59ff1eee08f639e6f1fb71d12d3ab4aea
Thunder <thunder20060629@gmail.com>
2026-03-30 21:17:42 +0800

    add preprocess and read data function

--- KNN/myUtil.py
+++ KNN/myUtil.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import numpy as np
 
-def read_data():
-    rData = pd.read_csv('WineQT.csv') # row data
+def read_data(fileName):
+    rData = pd.read_csv(fileName) # row data
     rX = rData.drop(columns=['quality', 'Id']).values # feature data
     rY = rData['quality'].to_numpy() # quality
     return rX,rY
+
+def preprocess1(rX,rY):
+    # normalize
+    X = (rX - rX.mean(axis=0))/rX.std(axis=0)
+    return X,rY
