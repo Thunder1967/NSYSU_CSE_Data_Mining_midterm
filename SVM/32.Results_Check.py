74c3cb1e7ca811d0223e903b73a6ea5ceda0f0d7
韷迌 Raku <160376059+Rakuusagi@users.noreply.github.com>
2026-04-26 18:04:39 +0800

    Reorganize

--- SVM/32.Results_Check.py
+++ SVM/32.Results_Check.py
@@ -1,8 +1,20 @@
 import pandas as pd
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
 from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt 
 import seaborn as sns
+
+def score(name, test_x, test_answer):
+    test_x_quality = test_x['quality']
+    test_answer_quality = test_answer['quality']
+    
+    print(f"▼//{name}Test Result")
+    print(f"Accuracy Score: {accuracy_score(test_answer_quality, test_x_quality)*100:.2f}%")
+    print(f"Precision Score: {precision_score(test_answer_quality, test_x_quality, zero_division=0, average='weighted')*100:.2f}%")
+    print(f"Recall Score: {recall_score(test_answer_quality, test_x_quality, average='weighted')*100:.2f}%")
+    print(f"F1 score: {f1_score(test_answer_quality, test_x_quality, average='weighted')*100:.2f}%")
+    print(f"Confusion Matrix:\n {confusion_matrix(test_answer_quality, test_x_quality)}")
+    print(f"")
 
 #資料讀取
 linear_pred = pd.read_csv("final_pred_linear.csv")
@@ -12,29 +24,11 @@ blend_pred = pd.read_csv("final_pred_blend.csv")
 test_data = pd.read_csv("test.csv")
 test_answer = test_data[['Id', 'quality']]
 
-#資料整理
-linear_pred = linear_pred.sort_values(by='Id', ascending=True, na_position='last')
-poly_pred = poly_pred.sort_values(by='Id', ascending=True, na_position='last')
-rbf_pred = rbf_pred.sort_values(by='Id', ascending=True, na_position='last')
-blend_pred = blend_pred.sort_values(by='Id', ascending=True, na_position='last')
-test_answer = test_answer.sort_values(by='Id', ascending=True, na_position='last')
-
-linear_pred_quality = linear_pred['quality']
-poly_pred_quality = poly_pred['quality']
-rbf_pred_quality = rbf_pred['quality']
-blend_pred_quality = blend_pred['quality']
-test_answer_quality = test_answer['quality']
-labels = [3, 4, 5, 6, 7, 8]
-
-#混淆矩陣
-cm_linear = confusion_matrix(test_answer_quality, linear_pred_quality, labels=labels)
-plt.figure(figsize=(8, 6))
-sns.heatmap(cm_linear, annot=True, fmt='d', cmap='Blues', xticklabels=labels, yticklabels=labels, vmin=0, vmax=100)
-plt.xlabel('Predicted Quality')
-plt.ylabel('True Quality')
-plt.title('Linear SVM Confusion Matrix')
-#plt.show()
-
+score("Linear", linear_pred, test_answer)
+score("Poly", poly_pred, test_answer)
+score("RBF", rbf_pred, test_answer)
+score("Blend", blend_pred, test_answer)
+"""
 cm_poly = confusion_matrix(test_answer_quality, poly_pred_quality, labels=labels)
 plt.figure(figsize=(8, 6))
 sns.heatmap(cm_poly, annot=True, fmt='d', cmap='Blues', xticklabels=labels, yticklabels=labels, vmin=0, vmax=100)
@@ -75,3 +69,4 @@ print(report_rbf)
 report_blend = classification_report(test_answer_quality, blend_pred_quality, digits=4, zero_division=0)
 print(f"blend Result: ")
 print(report_blend)
+"""
